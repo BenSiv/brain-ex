6a4e482ab35cd061390aac10a732cb911c8afab5
BenSiv <bensiv92@gmail.com>
2024-04-24 12:15:59 +0300

    improving note taking

--- src/note.lua
+++ src/note.lua
@@ -3,6 +3,34 @@ local note = {}
 
 local sqlite = require("sqlite3")
 
+function get_note_id(brain_file, note)
+    local db = sqlite.open(brain_file)
+
+    local result_rows = {}
+    local query = "SELECT time FROM notes WHERE name='" .. note .. "'"
+    for row in db:rows(query) do
+        table.insert(result_rows, row)
+    end
+
+    return result_rows[1]
+end
+
+function connect_notes(brain_file, note, links)
+    local insert_statement = "INSERT INTO connections (source, target) VALUES "
+    local source = get_note_id(brain_file, note)
+    for index, link in pairs(split(links, ", ")) do
+        local target = get_note_id(brain_file, link)
+        local statement_value = "('" .. source .. "', '" .. target .. "'), "
+        insert_statement = insert_statement .. statement_value
+    end
+    insert_statement = slice(insert_statement, 1, length(insert_statement)-2) .. ";"
+    
+    -- write note info
+    local db = sqlite.open(brain_file)
+    db:exec(insert_statement)
+    db:close()
+end
+
 function take_note(brain_file)
     -- get note info
     io.write("Context group: ")
@@ -11,23 +39,13 @@ function take_note(brain_file)
     local title = "'" .. io.read() .. "'"
     io.write("Content: ")
     local content = "'" .. io.read() .. "'"
+    io.write("Links: ")
+    local links = io.read()
+
+    connect_notes(brain_file, title, links)
+    write_note() -- write the markdwon content including timestamp
 
     local insert_statement = "INSERT INTO notes ('group', 'name', 'content') VALUES ('" .. group .. "', '" .. title .. "', '" .. content .. "');"
-
-    -- write note info
-    local db = sqlite.open(brain_file)
-    db:exec(insert_statement)
-    db:close()
-end
-
-function connect_notes(brain_file)
-    -- get note info
-    io.write("Source note id: ")
-    local source = "'" .. io.read() .. "'"
-    io.write("Target note id: ")
-    local target = "'" .. io.read() .. "'"
-
-    local insert_statement = "INSERT INTO connections (source, target) VALUES ('" .. source .. "', '" .. target .. "');"
 
     -- write note info
     local db = sqlite.open(brain_file)
