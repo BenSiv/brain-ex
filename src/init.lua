d08b8e4bb2864f91a9c8188509400a32228b0f59
BenSiv <bensiv92@gmail.com>
2024-06-13 10:11:02 +0300

    adding argparse

--- src/init.lua
+++ src/init.lua
@@ -4,7 +4,6 @@ local init = {}
 local lfs = require("lfs")
 local sqlite = require("sqlite3")
 local path = require("path")
-local vault_to_sql = require("vault_to_sql").vault_to_sql
 
 local function script_path()
     local file_path = arg[0]
@@ -20,9 +19,6 @@ function init_bx()
     local brain_path = current_dir .. "/" .. brain_name .. ".bx"
     local home_dir = os.getenv("HOME")
 
-    -- remove old brain_path if it exists
-    -- os.remove(brain_path)
-
     -- read sql init commands
     local bx_path = script_path()
     local init_file = io.open(bx_path .. "init_brain.sql", "r")
@@ -33,49 +29,12 @@ function init_bx()
     local db = sqlite.open(brain_path)
     db:exec(sql_commands)
     db:close()
-
-    -- store info in ~/.bx filr
-    local dot_file = io.open(home_dir .. "/.bx", "w")
-    dot_file:write("brain: " .. brain_path)
-    dot_file:close()
-end
-
-function init_bx_with_vault()
-    -- get database name
-    io.write("Vault path: ")
-    local vault_dir = io.read()
-    local current_dir = lfs.currentdir()
-    local brain_file = vault_dir .. ".bx"
-    local brain_path = path(current_dir, brain_file)
-    local vault_path = path(current_dir, vault_dir)
-    local home_dir = os.getenv("HOME")
-
-    -- remove old brain_path if it exists
-    -- os.remove(brain_path)
-
-    -- read sql init commands
-    local bx_path = script_path()
-    local init_file = io.open(bx_path .. "init_brain.sql", "r")
-    local sql_commands = init_file:read("*a")
-    init_file:close()
-
-    -- create database and tables
-    local db = sqlite.open(brain_path)
-    db:exec(sql_commands)
-    db:close()
-
-    vault_to_sql(vault_path, brain_path)
 
     -- store info in ~/.bx file
     local dot_file = io.open(home_dir .. "/.bx", "w")
-    dot_file:write("vault: " .. vault_path .. "\n")
     dot_file:write("brain: " .. brain_path)
     dot_file:close()
 end
-
-
-init.init_bx = init_bx
-init.init_bx_with_vault = init_bx_with_vault
 
 -- Export the module
 return init
--- src/note.lua
+++ src/note.lua
@@ -1,9 +1,18 @@
 -- Define a module table
 local note = {}
 
+require("utils").using("utils")
+using("argparse")
+
 local sqlite = require("sqlite3")
 local path = require("path")
-local get_vault_path = require("bx_utils").get_vault_path
+local get_vault_path = require("brex").get_vault_path
+
+local arg_string = [[
+    -a --add arg string false
+]]
+
+local expected_args = def_args(arg_string)
 
 local function get_note_id(brain_file, note)
     local db = sqlite.open(brain_file)
@@ -33,20 +42,9 @@ local function connect_notes(brain_file, title, links)
     db:close()
 end
 
-local function write_note(vault_dir, group, title, content, links)
-    local to_write = "content"
-    local obsidian_links = ""
-    for index, link in pairs(split(links, ", ")) do
-        obsidian_links = obsidian_links .. "[[" .. link .. "]] "
-    end
-    note_path = path(vault_dir, group, title .. ".md")
-    local note_file = io.open(note_path, "a")
-    note_file:write(content .. "\n")
-    note_file:write(obsidian_links)
-    note_file:close()
-end
-
 function take_note(brain_file)
+
+    local arg_parsed = parse_args(arg, expected_args)
     -- get note info
     io.write("Context group: ")
     local group = "'" .. io.read() .. "'"
@@ -60,7 +58,6 @@ function take_note(brain_file)
     local vault_dir = get_vault_path()
 
     connect_notes(brain_file, title, links)
-    write_note(vault_dir, group, title, content, links)
 
     local insert_statement = "INSERT INTO notes ('group', 'name', 'content') VALUES ('" .. group .. "', '" .. title .. "', '" .. content .. "');"
 
