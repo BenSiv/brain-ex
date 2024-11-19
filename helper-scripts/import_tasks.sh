d845fc9670c46c56c1dd03b9c8259e7813e2b9ff
bensiv <bensiv92@gmail.com>
2024-11-19 16:18:24 +0200

    updating task import script

--- helper-scripts/import_tasks.sh
+++ helper-scripts/import_tasks.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# Variables
-DB_FILE="../obsidian-work.db"  # Path to the SQLite database
-TSV_FILE="tasks.tsv"           # Path to the TSV file to import
-TABLE_NAME="todos"             # Table to import the data into
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <db_file> <tsv_file>"
+    exit 1
+fi
 
-# Run the SQLite command
+DB_FILE="$1"
+TSV_FILE="$2"
+TABLE_NAME="tasks"
+
 sqlite3 "$DB_FILE" <<EOF
 .mode tabs
 .import $TSV_FILE $TABLE_NAME
 .quit
 EOF
-
-echo "Data imported from $TSV_FILE into $TABLE_NAME table"
